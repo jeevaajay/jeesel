6369632df2f1a79219b97ad3b71a71f7e7b11aab
jeevaajay <30457764+jeevaajay@users.noreply.github.com>
2017-07-26 14:38:11 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  int num,count=0;
+  printf("Enter a number: ");
+  scanf("%d",&num);
+  while(num)
+  {
+      num=num/10;
+      count++;
+  }
+  printf("Total digits is:  %d",count);
+  return 0;
+}
